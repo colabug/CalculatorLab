ab7696a6e76d92d0836dd963a2aa7fec4d2550ae
Corey Leigh Latislaw <colabug@gmail.com>
2014-07-10 21:23:11 -0400

    Robolectric Integration, Step 1: Hello World

---
+++ src/com/mobiquity/calculator/CalculatorActivity.java
@@ -0,0 +1,17 @@
+package com.mobiquity.calculator;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CalculatorActivity extends Activity
+{
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate( Bundle savedInstanceState )
+    {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.main );
+    }
+}
