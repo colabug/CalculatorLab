7c6d87583e677e3eafa77ffd554a3ac5ffcb5117
Corey Leigh Latislaw <colabug@gmail.com>
2014-07-10 21:59:36 -0400

    First Test, Step 2: Added test runner

--- src/test/java/com/mobiquity/calculator/CalculatorActivityTest.java
+++ src/test/java/com/mobiquity/calculator/CalculatorActivityTest.java
@@ -1,8 +1,14 @@
 package com.mobiquity.calculator;
 
+import org.junit.runner.RunWith;
+
+import org.robolectric.RobolectricTestRunner;
+
 /**
  * @since 1.0
  */
+@RunWith (RobolectricTestRunner.class)
+
 public class CalculatorActivityTest
 {
 }
