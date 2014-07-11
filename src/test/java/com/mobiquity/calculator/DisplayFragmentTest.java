af3f91d6d6212a17dfc298fad2e50ee7e92a337d
Corey Leigh Latislaw <colabug@gmail.com>
2014-07-11 18:12:42 -0400

    Added display
    * Added tests for the display
    * Added edit text to the view
    * Added color file

--- src/test/java/com/mobiquity/calculator/DisplayFragmentTest.java
+++ src/test/java/com/mobiquity/calculator/DisplayFragmentTest.java
@@ -1,13 +1,19 @@
 package com.mobiquity.calculator;
 
+import android.widget.EditText;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.robolectric.RobolectricTestRunner;
 
+import static com.mobiquity.support.Assert.assertViewIsVisible;
 import static com.mobiquity.support.FragmentUtil.startFragment;
 
+import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
 
 /**
  * @since 1.0
@@ -16,11 +22,27 @@ import static org.junit.Assert.assertNotNull;
 
 public class DisplayFragmentTest
 {
+    private DisplayFragment fragment;
+
+    @Before
+    public void setUp() throws Exception
+    {
+        fragment = new DisplayFragment();
+        startFragment( fragment );
+    }
+
     @Test
     public void shouldNotBeNull() throws Exception
     {
-        DisplayFragment fragment = new DisplayFragment();
-        startFragment( fragment);
         assertNotNull( fragment );
+    }
+
+    @Test
+    public void shouldHaveDefaultDisplay() throws Exception
+    {
+        EditText display = (EditText) fragment.getView().findViewById( R.id.display );
+        assertViewIsVisible( display );
+        assertThat( display.getText().toString(),
+                    equalTo( "" ) );
     }
 }
