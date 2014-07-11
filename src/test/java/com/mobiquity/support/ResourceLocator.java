d25172d945dcc50d63cb485ed6e41ba3edda1681
Corey Leigh Latislaw <colabug@gmail.com>
2014-07-10 22:35:30 -0400

    Best Practices, Utility Functions: Added Assert and ResourceLocator

---
+++ src/test/java/com/mobiquity/support/ResourceLocator.java
@@ -0,0 +1,18 @@
+package com.mobiquity.support;
+
+import android.graphics.drawable.Drawable;
+
+import org.robolectric.Robolectric;
+
+public class ResourceLocator
+{
+    public static String getString( int stringId )
+    {
+        return Robolectric.application.getString( stringId );
+    }
+
+    public static Drawable getDrawable( int drawableId )
+    {
+        return Robolectric.application.getResources().getDrawable( drawableId );
+    }
+}
