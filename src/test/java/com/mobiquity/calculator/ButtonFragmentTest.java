b38d3f6f8676eb4268d0db2bf704d9f1f91dcf39
Corey Leigh Latislaw <colabug@gmail.com>
2014-07-15 19:30:55 -0400

    REFACTOR: Suggested in class to use a loop
    * Created a custom class
    * Created list of english buttons and expected values
    * Created list of japanese buttons and expected values
    * Looped through each list to verify the values
    * Removed excess code

--- src/test/java/com/mobiquity/calculator/ButtonFragmentTest.java
+++ src/test/java/com/mobiquity/calculator/ButtonFragmentTest.java
@@ -1,6 +1,5 @@
 package com.mobiquity.calculator;
 
-import android.view.View;
 import android.widget.Button;
 
 import org.junit.Before;
@@ -9,6 +8,8 @@ import org.junit.runner.RunWith;
 
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
+
+import java.util.ArrayList;
 
 import static com.mobiquity.support.Assert.assertViewIsVisible;
 import static com.mobiquity.support.FragmentUtil.startFragment;
@@ -26,6 +27,20 @@ public class ButtonFragmentTest
 {
     private ButtonFragment buttonFragment;
 
+    private class ButtonTestValues
+    {
+        int    id;
+        int    stringId;
+        String expectedString;
+
+        public ButtonTestValues( int id, int stringId, String expectedString )
+        {
+            this.id = id;
+            this.stringId = stringId;
+            this.expectedString = expectedString;
+        }
+    }
+
     @Before
     public void setUp() throws Exception
     {
@@ -40,210 +55,77 @@ public class ButtonFragmentTest
     }
 
     @Test
-    public void shouldHave1Key() throws Exception
+    public void shouldVerifyEnglishButtons() throws Exception
     {
-        verifyKey( R.id.key1, R.string.key1, "1" );
+        for ( ButtonTestValues button : createEnglishList() )
+        {
+            verifyButton( button.id,
+                          button.stringId,
+                          button.expectedString );
+        }
+    }
+
+    private ArrayList<ButtonTestValues> createEnglishList()
+    {
+        ArrayList<ButtonTestValues> englishList = new ArrayList<ButtonTestValues>();
+
+        englishList.add( new ButtonTestValues( R.id.key1, R.string.key1, "1" ) );
+        englishList.add( new ButtonTestValues( R.id.key2, R.string.key2, "2" ) );
+        englishList.add( new ButtonTestValues( R.id.key3, R.string.key3, "3" ) );
+        englishList.add( new ButtonTestValues( R.id.key4, R.string.key4, "4" ) );
+        englishList.add( new ButtonTestValues( R.id.key5, R.string.key5, "5" ) );
+        englishList.add( new ButtonTestValues( R.id.key6, R.string.key6, "6" ) );
+        englishList.add( new ButtonTestValues( R.id.key7, R.string.key7, "7" ) );
+        englishList.add( new ButtonTestValues( R.id.key8, R.string.key8, "8" ) );
+        englishList.add( new ButtonTestValues( R.id.key9, R.string.key9, "9" ) );
+        englishList.add( new ButtonTestValues( R.id.key0, R.string.key0, "0" ) );
+        englishList.add( new ButtonTestValues( R.id.plus, R.string.plus, "+" ) );
+        englishList.add( new ButtonTestValues( R.id.minus, R.string.minus, "-" ) );
+        englishList.add( new ButtonTestValues( R.id.multiply, R.string.multiply, "*" ) );
+        englishList.add( new ButtonTestValues( R.id.divide, R.string.divide, "/" ) );
+        englishList.add( new ButtonTestValues( R.id.modulo, R.string.modulo, "%" ) );
+        englishList.add( new ButtonTestValues( R.id.equal, R.string.equal, "=" ) );
+        englishList.add( new ButtonTestValues( R.id.clear, R.string.clear, "C" ) );
+
+        return englishList;
     }
 
     @Test
     @Config (qualifiers = "ja")
-    public void shouldHaveJa1Key()
+    public void shouldHaveJapaneseButtons()
     {
-        verifyKey( R.id.key1, R.string.key1, "一" );
+        ArrayList<ButtonTestValues> japaneseList = createJapaneseList();
+        for ( ButtonTestValues e : japaneseList )
+        {
+            verifyButton( e.id, e.stringId, e.expectedString );
+        }
     }
 
-    @Test
-    public void shouldHave2Key() throws Exception
+    private ArrayList<ButtonTestValues> createJapaneseList()
     {
-        verifyKey( R.id.key2, R.string.key2, "2" );
+        ArrayList<ButtonTestValues> japaneseList = new ArrayList<ButtonTestValues>();
+
+        japaneseList.add( new ButtonTestValues( R.id.key1, R.string.key1, "一" ) );
+        japaneseList.add( new ButtonTestValues( R.id.key2, R.string.key2, "二" ) );
+        japaneseList.add( new ButtonTestValues( R.id.key3, R.string.key3, "三" ) );
+        japaneseList.add( new ButtonTestValues( R.id.key4, R.string.key4, "四" ) );
+        japaneseList.add( new ButtonTestValues( R.id.key5, R.string.key5, "五" ) );
+        japaneseList.add( new ButtonTestValues( R.id.key6, R.string.key6, "六" ) );
+        japaneseList.add( new ButtonTestValues( R.id.key7, R.string.key7, "七" ) );
+        japaneseList.add( new ButtonTestValues( R.id.key8, R.string.key8, "八" ) );
+        japaneseList.add( new ButtonTestValues( R.id.key9, R.string.key9, "九" ) );
+        japaneseList.add( new ButtonTestValues( R.id.key0, R.string.key0, "零" ) );
+
+        return japaneseList;
     }
 
-    @Test
-    @Config (qualifiers = "ja")
-    public void shouldHaveJa2Key()
+    private void verifyButton( int id, int stringId, String expectedString )
     {
-        verifyKey( R.id.key2, R.string.key2, "二");
-    }
-
-    @Test
-    public void shouldHave3Key() throws Exception
-    {
-        verifyKey( R.id.key3, R.string.key3, "3");
-    }
-
-    @Test
-    @Config (qualifiers = "ja")
-    public void shouldHaveJa3Key()
-    {
-        verifyKey( R.id.key3, R.string.key3, "三");
-    }
-
-    @Test
-    public void shouldHave4Key() throws Exception
-    {
-        verifyKey( R.id.key4, R.string.key4, "4");
-    }
-
-    @Test
-    @Config (qualifiers = "ja")
-    public void shouldHaveJa4Key()
-    {
-        verifyKey( R.id.key4, R.string.key4, "四");
-    }
-
-    @Test
-    public void shouldHave5Key() throws Exception
-    {
-        verifyKey( R.id.key5, R.string.key5, "5");
-    }
-
-    @Test
-    @Config (qualifiers = "ja")
-    public void shouldHaveJa5Key()
-    {
-        verifyKey( R.id.key5, R.string.key5, "五");
-    }
-
-    @Test
-    public void shouldHave6Key() throws Exception
-    {
-        verifyKey( R.id.key6, R.string.key6, "6");
-    }
-
-    @Test
-    @Config (qualifiers = "ja")
-    public void shouldHaveJa6Key()
-    {
-        verifyKey( R.id.key6, R.string.key6, "六");
-    }
-
-    @Test
-    public void shouldHave7Key() throws Exception
-    {
-        verifyKey( R.id.key7, R.string.key7, "7");
-    }
-    @Test
-    @Config (qualifiers = "ja")
-    public void shouldHaveJa7Key()
-    {
-        verifyKey( R.id.key7, R.string.key7, "七");
-    }
-
-    @Test
-    public void shouldHave8Key() throws Exception
-    {
-        verifyKey( R.id.key8, R.string.key8, "8");
-    }
-    @Test
-    @Config (qualifiers = "ja")
-    public void shouldHaveJa8Key()
-    {
-        verifyKey( R.id.key8, R.string.key8, "八");
-    }
-
-    @Test
-    public void shouldHave9Key() throws Exception
-    {
-        verifyKey( R.id.key9, R.string.key9, "9");
-    }
-
-    @Test
-    @Config (qualifiers = "ja")
-    public void shouldHaveJa9Key()
-    {
-        verifyKey( R.id.key9, R.string.key9, "九");
-    }
-
-    @Test
-    public void shouldHave0Key() throws Exception
-    {
-        verifyKey( R.id.key0, R.string.key0, "0");
-    }
-
-    @Test
-    @Config (qualifiers = "ja")
-    public void shouldHaveJa0Key()
-    {
-        verifyKey( R.id.key0, R.string.key0, "零" );
-    }
-
-    private void verifyKey( int id, int stringId, String expectedString )
-    {
-        Button key = (Button) getViewById( id );
+        Button key = (Button) buttonFragment.getView().findViewById( id );
         assertViewIsVisible( key );
 
         String resourceString = getString( stringId );
-        assertThat( key.getText().toString(),
-                    equalTo( resourceString ) );
-        assertThat( expectedString,
-                    equalTo( resourceString ) );
-    }
-
-    @Test
-    public void shouldHavePlusKey() throws Exception
-    {
-        Button plus = (Button) getViewById( R.id.plus );
-        assertViewIsVisible( plus );
-        assertThat( plus.getText().toString(),
-                    equalTo( getString( R.string.plus ) ) );
-    }
-
-    @Test
-    public void shouldHaveMinusKey() throws Exception
-    {
-        Button minus = (Button) getViewById( R.id.minus );
-        assertViewIsVisible( minus );
-        assertThat( minus.getText().toString(),
-                    equalTo( getString( R.string.minus ) ) );
-    }
-
-    @Test
-    public void shouldHaveMultiplyKey() throws Exception
-    {
-        Button multiply = (Button) getViewById( R.id.multiply );
-        assertViewIsVisible( multiply );
-        assertThat( multiply.getText().toString(),
-                    equalTo( getString( R.string.multiply ) ) );
-    }
-
-    @Test
-    public void shouldHaveDivideKey() throws Exception
-    {
-        Button divide = (Button) getViewById( R.id.divide );
-        assertViewIsVisible( divide );
-        assertThat( divide.getText().toString(),
-                    equalTo( getString( R.string.divide ) ) );
-    }
-
-    @Test
-    public void shouldHaveModuloKey() throws Exception
-    {
-        Button modulo = (Button) getViewById( R.id.modulo );
-        assertViewIsVisible( modulo );
-        assertThat( modulo.getText().toString(),
-                    equalTo( getString( R.string.modulo ) ) );
-    }
-
-    @Test
-    public void shouldHaveEqualKey() throws Exception
-    {
-        Button equal = (Button) getViewById( R.id.equal );
-        assertViewIsVisible( equal );
-        assertThat( equal.getText().toString(),
-                    equalTo( getString( R.string.equal ) ) );
-    }
-
-    @Test
-    public void shouldHaveClearKey() throws Exception
-    {
-        Button clear = (Button) getViewById( R.id.clear );
-        assertViewIsVisible( clear );
-        assertThat( clear.getText().toString(),
-                    equalTo( getString( R.string.clear ) ) );
-    }
-
-    private View getViewById( int id )
-    {
-        return buttonFragment.getView().findViewById( id );
+        assertThat( key.getText().toString(), equalTo( resourceString ) );
+        assertThat( expectedString, equalTo( resourceString ) );
     }
 }
